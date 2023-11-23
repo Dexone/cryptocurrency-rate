aa9122d03ad915e3e52e1529b820e8aecf7ed7b8
Dmitriy Martynov <dm346@mail.ru>
2023-11-23 13:43:11 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -23,107 +23,71 @@ ChartJS.register(
 
 
 function ValutePicker() {
-  console.log("test")
-    const selectValue = useRef(0)
-
-    const handleChange = (value) => {
-      selectValue.current = { value }
-    };
 
 
-    const [selectedValute, setSelectedValute] = useState("bitcoin"); //select
+  const [selectedValute, setSelectedValute] = useState("bitcoin"); //select
 
 
-    axios.get(`https://api.coincap.io/v2/assets/${selectedValute}/history?interval=h1`).then((res) => {
-      console.log(selectedValute)
-      dataLabels.value = [res.data.data[715].date.slice(11, 16), res.data.data[716].date.slice(11, 16), res.data.data[717].date.slice(11, 16), res.data.data[718].date.slice(11, 16), res.data.data[719].date.slice(11, 16)]
-      dataValues.value = [res.data.data[715].priceUsd, res.data.data[716].priceUsd, res.data.data[717].priceUsd, res.data.data[718].priceUsd, res.data.data[719].priceUsd]
-    })
+  axios.get(`https://api.coincap.io/v2/assets/${selectedValute}/history?interval=h1`).then((res) => {
+    console.log(selectedValute)
+    dataLabels.value = [res.data.data[715].date.slice(11, 16), res.data.data[716].date.slice(11, 16), res.data.data[717].date.slice(11, 16), res.data.data[718].date.slice(11, 16), res.data.data[719].date.slice(11, 16)]
+    dataValues.value = [res.data.data[715].priceUsd, res.data.data[716].priceUsd, res.data.data[717].priceUsd, res.data.data[718].priceUsd, res.data.data[719].priceUsd]
+  })
 
 
 
 
-    const dataLabels = useRef([])
-    const dataValues = useRef([])
+  const dataLabels = useRef([])
+  const dataValues = useRef([])
 
 
 
-    const data = {
-      labels: dataLabels.value,
-      datasets: [
-        {
-          data: dataValues.value,
-          backgroundColor: '#333639',
-          borderColor: '#333639',
-        }
-      ]
-    }
-    const options = {
-      plugins: {
-        legend: true
+  const data = {
+    labels: dataLabels.value,
+    datasets: [
+      {
+        data: dataValues.value,
+        backgroundColor: '#333639',
+        borderColor: '#333639',
       }
-    }
-
-
-    function test() {
-
-    }
-
-
-
-
-
-    return (
-      <>
-        <div className='ValutePicker'>
-
-
-
-          <Select
-            defaultValue="bitcoin"
-            style={{
-              width: 300,
-            }}
-            onChange={handleChange}
-            options={[
-              {
-                value: 'bitcoin',
-                label: 'Bitcoin',
-              },
-              {
-                value: 'ethereum',
-                label: 'Ethereum',
-              },
-              {
-                value: 'litecoin',
-                label: 'Litecoin',
-              },
-            ]}
-          />
-
-
-
-          <button onClick={e => (setSelectedValute(e.target.value))}>test</button>
-
-          <select
-            value={selectedValute}
-            onChange={e => setSelectedValute(e.target.value)}
-          >
-            <option value="bitcoin">Bitcoin</option>
-            <option value="ethereum">Ethereum</option>
-            <option value="litecoin">Litecoin</option>
-          </select>
-
-          <div className="App">
-            <Line
-              data={data}
-              options={options}
-            ></Line>
-          </div>
-        </div>
-      </>
-    );
+    ]
   }
+  const options = {
+    plugins: {
+      legend: true
+    }
+  }
+
+
+
+
+
+
+
+  return (
+    <>
+      <div className='ValutePicker'>
+
+
+        <select
+          value={selectedValute}
+          onChange={e => setSelectedValute(e.target.value)}
+        >
+          <option value="bitcoin">Bitcoin</option>
+          <option value="ethereum">Ethereum</option>
+          <option value="litecoin">Litecoin</option>
+        </select>
+
+        <div className="App">
+          <Line
+            data={data}
+            options={options}
+          ></Line>
+        </div>
+      </div>
+    </>
+  );
+}
 export default ValutePicker;
 
 
